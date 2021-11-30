7ae6382e893eabf946e5a5bb617bb063d6b750e0
Anton Strogonoff <anton@strogonoff.name>
2021-11-30 21:52:26 +0100

    docs(download): remove outdated point (#1)

--- src/download.ts
+++ src/download.ts
@@ -35,7 +35,6 @@ function isValidLFSInfoResponseData(val: Record<string, any>): val is LFSInfoRes
 
 /**
  * Downloads, caches and returns a blob corresponding to given LFS pointer.
- * Currently, the authorization header is responsibility of the caller.
  */
 export default async function downloadBlobFromPointer(
   { http: { request }, headers = {}, url, auth }: DownloadBlobRequest,
