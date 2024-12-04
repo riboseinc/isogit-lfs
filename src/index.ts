5daad860f712a6d748a7227518dd75480beb62b2
Anton Strogonoff <anton@strogonoff.name>
2024-12-04 19:52:19 +0800

    feat: export `pointsToLFS()` as part of main API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as downloadBlobFromPointer } from './download';
 export { default as populateCache } from './populateCache';
 export { readPointer } from './pointers';
+export { pointsToLFS } from './util';
